96fd247a9b3c2e31e91a8a9008fb047d900b2aff
TakahashiTakuma <takuma521521@gmail.com>
2020-10-03 14:57:58 +0900

    Separate output settings into outputs.tf

--- main.tf
+++ main.tf
@@ -101,9 +101,3 @@ resource "aws_instance" "cm-test" {
     Name = "cm-test"
   }
 }
-
-# EC2インスタンスのパブリックIPなど、環境を構築した結果リソースに割り当てられた属性値を知りたい場合
-# terraformコマンド実行時に指定した属性値がコンソール上に出力される
-output "public_ip_of_cm-test" {
-  value = aws_instance.cm-test.public_ip
-}
